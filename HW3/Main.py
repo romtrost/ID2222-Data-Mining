eded58b7bf9ae726d8a71aaeaabef26b56032aea
emotror <romain.trost@ericsson.com>
2023-12-01 13:48:20 +0100

    added triest-impr

--- HW3/Main.py
+++ HW3/Main.py
@@ -1,21 +1,24 @@
 import time
 import argparse
 from TriestBase import TriestBase
+from TriestImpr import TriestImpr
 from DataPreprocessing import read_dataset
 
 parser = argparse.ArgumentParser(description='Finding triangle count estimates in a data stream of graph edges using TRIEST.')
 parser.add_argument('-dataset-name', default='data/web-Stanford.txt', help='path to dataset')
 parser.add_argument('-m', default=10000, type=int, help='how many edge samples to use in reservoir sampling')
+parser.add_argument('-t', default='BASE', type=str, help='type of TRIEST algorithm to use: [BASE, IMPR]')
 
 args = parser.parse_args()
 print(args)
 
 stream_edges_dataset = read_dataset(args.dataset_name)
 
-print("--- Finding triangle estimates using TRIEST ---")
-triest = TriestBase(args.m)
+triest = TriestBase(args.m) if args.t == 'BASE' else TriestImpr(args.m)
+
+print(f"--- Finding triangle estimates using TRIEST-{args.t} ---")
 start_time = time.time()
 glo_triangles, loc_triangles = triest.algorithm(stream_edges_dataset)
 print(f'Global triangles estimate: {glo_triangles}')
-print(f'Local triangles estimate: {loc_triangles}')
+#print(f'Local triangles estimate: {loc_triangles}')
 print(f'The algorithm took {time.time() - start_time} seconds.')
--- HW3/TriestImpr.py
+++ HW3/TriestImpr.py
@@ -2,7 +2,7 @@ import random
 from collections import defaultdict
 from SubGraph import SubGraph
 
-class TriestBase:
+class TriestImpr:
     """ Implementation of the Triest-Base Algorithm. """
 
     def __init__(self, m):
@@ -21,20 +21,24 @@ class TriestBase:
                 x, y = random.choice(list(self.subgraph.edges))
                 # remove sampled edge from graph
                 self.subgraph.rem_edge(x, y)
-                self.update_counters(x, y, -1)
                 return True
             else:
                 return False
         else:
            return True
 
-    def update_counters(self, u, v, value):
+    def update_counters(self, u, v, t):
         """ Update counters. """
 
         # need edge to contain both points
         if u in self.subgraph.adjency and v in self.subgraph.adjency:
 
             neighbors = self.subgraph.adjency.get(u) & self.subgraph.adjency.get(v)
+
+            if t*(t-1)*(t-2) / (self.m*(self.m-1)*(self.m-2)) > 1:
+                value = t*(t-1)*(t-2) / (self.m*(self.m-1)*(self.m-2))
+            else:
+                value = 1
 
             # update the global/local counters
             for neighbor in neighbors:
@@ -43,49 +47,23 @@ class TriestBase:
                 self.loc_count[u] += value
                 self.loc_count[v] += value
 
-            if value == -1:
-                for node in neighbors.union({u, v}):
-                    if not self.loc_count[node]:
-                        del self.loc_count[node]
-
-            """
-            if neighbors:
-                print("u:                 ", u)
-                print("v:                 ", v)
-                print("value:                 ", value)
-                print("self.subgraph.adjency.get(u)", self.subgraph.adjency.get(u))
-                print("self.subgraph.adjency.get(v)", self.subgraph.adjency.get(v))
-                print("neighbors", neighbors)
-                print("self.glo_count:    ", self.glo_count)
-                print("self.loc_count:    ", self.loc_count)
-            """
-
     def algorithm(self, stream_edges_dataset):
         """ TRIEST algorithm. """
 
         t = 0
         for u, v in stream_edges_dataset:
             # make sure this edge is not in our subgraph
-            #print("t ---------------------------> ", t)
             if not (u, v) in self.subgraph.edges:
                 t += 1
+                self.update_counters(u, v, t)
                 is_sampled = self.reservoir_sample(t)
-                #print("is_sampled: ", is_sampled)
                 if is_sampled:
                     self.subgraph.add_edge(u, v)
-                    self.update_counters(u, v, 1)
-            #print("subgraph.adjency:  ", self.subgraph.adjency)
-            #print("subgraph.edges:    ", self.subgraph.edges)
 
         # compute global and local triangle count estimates
-        if t*(t-1)*(t-2) / (self.m*(self.m-1)*(self.m-2)) > 1:
-            est_t = t*(t-1)*(t-2) / (self.m*(self.m-1)*(self.m-2))
-        else:
-            est_t = 1
-
-        glo_triangles = int(est_t * self.glo_count)
+        glo_triangles = int(self.glo_count)
         loc_triangles = {}
         for node, counter in self.loc_count.items():
-            loc_triangles[node] = int(est_t * counter)
+            loc_triangles[node] = int(counter)
 
         return glo_triangles, loc_triangles
